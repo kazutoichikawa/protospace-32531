76409dd17ce5bfedf347781b285de951d9a51247
kazutoichikawa <nekomaru33281124@yahoo.co.jp>
2020-12-14 13:33:35 +0900

    initial commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,60 @@
+class PrototypesController < ApplicationController
+  before_action :set_prototype, only: [:show, :edit]
+  before_action :authenticate_user!, only: [:index]
+  before_action :move_to_index, only: [:edit]
+
+  def index
+    @prototypes = Prototype.all.order("created_at DESC")
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+
+  def create
+    Prototype.create(prototype_params)
+    redirect_to root_path
+  end
+
+  def show
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+  end
+
+  def update
+    @prototype = Prototype.find(params[:id])
+    @prototype.update(prototype_params)
+    if @prototype.save
+      redirect_to "/prototypes/#{@prototype.id}"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @prototype = Prototype.find(params[:id])
+    @prototype.destroy
+    redirect_to root_path
+  end
+
+
+  private
+  def prototype_params
+    params.require(:prototype).permit(:name, :title, :catch_copy, :concept, :image, :text).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in? && current_user.id == @prototype.user.id
+      redirect_to action: :index
+    end
+  end
+  
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  has_many :prototypes
+  has_many :comments
+
+  validates :name, presence: true
+  validates :occupation, presence: true
+  validates :position, presence: true
+  validates :profile, presence: true
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
